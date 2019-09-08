809d5824577c16d28aaf952752b3751c829137dc
iamcoderloser <642872728@qq.com>
2019-09-08 17:38:03 +0800

    导入初始工程

---
+++ src/components/Customers.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="customers container">
+    <h1 class="page-header">用户管理系统</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>姓名</th>
+          <th>电话</th>
+          <th>邮箱</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers" :key="customer.id">
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default { 
+  name: 'customers',
+  data () {
+    return {
+      customers: []
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
